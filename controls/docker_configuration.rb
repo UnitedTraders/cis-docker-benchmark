e9c60760fffac8aa249eca39a220026c2bcecb40
Vladimir Antipov <v.antipov@unitedtraders.com>
2020-06-23 05:23:23 +0300

    Fixed test numbers

--- controls/docker_configuration.rb
+++ controls/docker_configuration.rb
@@ -31,18 +31,12 @@ end
 
 # Docker Daemon Security Configuration Tests
 
-control 'docker-2.1' do
+control 'docker-1.1' do
   impact 1.0
   title 'Verify that docker.service file ownership is set to root:root'
   desc 'Verify that the \'docker.service\' file ownership and group-ownership are correctly set to \'root\'.
 
   Rationale: \'docker.service\' file contains sensitive parameters that may alter the behavior of Docker daemon. Hence, it should be owned and group-owned by \'root\' to maintain the integrity of the file.'
-
-  tag 'docker'
-  tag 'cis-docker-1.12.0': '2.1'
-  tag 'cis-docker-1.13.0': '2.1'
-  tag 'level:1'
-  ref 'Control and configure Docker with systemd', url: 'https://docs.docker.com/engine/admin/systemd/'
 
   describe file(DOCKER_SERVICE_PATH) do
     it { should exist }
@@ -52,18 +46,12 @@ control 'docker-2.1' do
   end
 end
 
-control 'docker-2.2' do
+control 'docker-1.2' do
   impact 1.0
   title 'Verify that docker.service file permissions are set to 644 or more restrictive'
   desc 'Verify that the \'docker.service\' file permissions are correctly set to \'644\' or more restrictive.
 
   Rationale: \'docker.service\' file contains sensitive parameters that may alter the behavior of Docker daemon. Hence, it should not be writable by any other user other than \'root\' to maintain the integrity of the file.'
-
-  tag 'docker'
-  tag 'cis-docker-1.12.0': '2.2'
-  tag 'cis-docker-1.13.0': '2.2'
-  tag 'level:1'
-  ref 'Control and configure Docker with systemd', url: 'https://docs.docker.com/engine/admin/systemd/'
 
   describe file(DOCKER_SERVICE_PATH) do
     it { should exist }
@@ -78,18 +66,12 @@ control 'docker-2.2' do
   end
 end
 
-control 'docker-2.3' do
+control 'docker-1.3' do
   impact 1.0
   title 'Verify that /etc/docker directory ownership is set to root:root'
   desc '\'/etc/docker\' directory contains certificates and keys in addition to various sensitive files. Hence, it should be owned and group-owned by \'root\' to maintain the integrity of the directory.
 
   Rationale: \'/etc/docker\' directory contains certificates and keys in addition to various sensitive files. Hence, it should be owned and group-owned by \'root\' to maintain the integrity of the directory.'
-
-  tag 'docker'
-  tag 'cis-docker-1.12.0': '2.3'
-  tag 'cis-docker-1.13.0': '2.3'
-  tag 'level:1'
-  ref 'Protect the Docker daemon socket', url: 'https://docs.docker.com/engine/security/https/'
 
   describe file('/etc/docker') do
     it { should exist }
@@ -99,18 +81,12 @@ control 'docker-2.3' do
   end
 end
 
-control 'docker-2.4' do
+control 'docker-1.4' do
   impact 1.0
   title 'Verify that /etc/docker directory permissions are set to 755 or more restrictive'
   desc 'Verify that the /etc/docker directory permissions are correctly set to \'755\' or more restrictive.
 
   Rationale: \'/etc/docker\' directory contains certificates and keys in addition to various sensitive files. Hence, it should only be writable by \'root\' to maintain the integrity of the directory.'
-
-  tag 'docker'
-  tag 'cis-docker-1.12.0': '2.4'
-  tag 'cis-docker-1.13.0': '2.4'
-  tag 'level:1'
-  ref 'Docker Security', url: 'https://docs.docker.com/engine/security/security/#conclusions'
 
   describe file('/etc/docker') do
     it { should exist }
@@ -127,19 +103,12 @@ control 'docker-2.4' do
   end
 end
 
-control 'docker-2.5' do
+control 'docker-1.5' do
   impact 1.0
   title 'Verify that Docker socket file ownership is set to root:docker'
   desc 'Verify that the Docker socket file is owned by \'root\' and group-owned by \'docker\'.
 
   Rationale: Docker daemon runs as \'root\'. The default Unix socket hence must be owned by \'root\'. If any other user or process owns this socket, then it might be possible for that non-privileged user or process to interact with Docker daemon. Also, such a non-privileged user or process might interact with containers. This is neither secure nor desired behavior. Additionally, the Docker installer creates a Unix group called \'docker\'. You can add users to this group, and then those users would be able to read and write to default Docker Unix socket. The membership to the \'docker\' group is tightly controlled by the system administrator. If any other group owns this socket, then it might be possible for members of that group to interact with Docker daemon. Also, such a group might not be as tightly controlled as the \'docker\' group. This is neither secure nor desired behavior. Hence, the default Docker Unix socket file must be owned by \'root\' and group-owned by \'docker\' to maintain the integrity of the socket file.'
-
-  tag 'docker'
-  tag 'cis-docker-1.12.0': '2.5'
-  tag 'cis-docker-1.13.0': '2.5'
-  tag 'level:1'
-  ref 'Use the Docker command line', url: 'https://docs.docker.com/engine/reference/commandline/cli/#daemon-socket-option'
-  ref 'Protect the Docker daemon socket', url: 'https://docs.docker.com/engine/security/https/'
 
   describe file('/var/run/docker.sock') do
     it { should exist }
@@ -149,19 +118,12 @@ control 'docker-2.5' do
   end
 end
 
-control 'docker-2.6' do
+control 'docker-1.6' do
   impact 1.0
   title 'Verify that Docker socket file permissions are set to 660 or more restrictive'
   desc 'Only \'root\' and members of \'docker\' group should be allowed to read and write to default Docker Unix socket. Hence, the Docket socket file must have permissions of \'660\' or more restrictive.
 
   Rationale: Only \'root\' and members of \'docker\' group should be allowed to read and write to default Docker Unix socket. Hence, the Docket socket file must have permissions of \'660\' or more restrictive.'
-
-  tag 'docker'
-  tag 'cis-docker-1.12.0': '2.6'
-  tag 'cis-docker-1.13.0': '2.6'
-  tag 'level:1'
-  ref 'Use the Docker command line', url: 'https://docs.docker.com/engine/reference/commandline/cli/#daemon-socket-option'
-  ref 'Protect the Docker daemon socket', url: 'https://docs.docker.com/engine/security/https/'
 
   describe file('/var/run/docker.sock') do
     it { should exist }
--- controls/host_configuration.rb
+++ controls/host_configuration.rb
@@ -36,10 +36,6 @@ control 'host-1.1' do
   impact 1.0
   title 'Use the updated Linux Kernel'
   desc 'Docker in daemon mode has specific kernel requirements. A 3.10 Linux kernel is the minimum requirement for Docker.'
-
-  tag 'host'
-  tag 'cis-docker-1.12.0': '1.2'
-  tag 'level:1'
   
   only_if { os.linux? }
   kernel_version = command('uname -r | grep -o \'^\w\.\w*\.\w*\'').stdout
@@ -55,11 +51,6 @@ control 'host-1.2' do
   desc 'There are frequent releases for Docker software that address security vulnerabilities,product bugs and bring in new functionality. Keep a tab on these product updates and upgrade as frequently as when new security vulnerabilities are fixed or deemed correct for your organization.
 
   Rationale: By staying up to date on Docker updates, vulnerabilities in the Docker software can be mitigated. An educated attacker may exploit known vulnerabilities when attempting to attain access or elevate privileges. Not installing regular Docker updates may leave you ith running vulnerable Docker software. It might lead to elevation privileges, unauthorized access or other security breaches. Keep a track of new releases and update as necessary.'
-
-  tag 'host'
-  tag 'cis-docker-1.12.0': '1.5'
-  tag 'cis-docker-1.13.0': '1.3'
-  tag 'level:1'
   
   docker_server_version = command('docker version --format \'{{.Server.Version}}\'').stdout
   docker_client_version = command('docker version --format \'{{.Client.Version}}\'').stdout
@@ -79,12 +70,6 @@ control 'host-1.3' do
 
   Rationale: Apart from auditing your regular Linux file system and system calls, audit Docker daemon as well. Docker daemon runs with \'root\' privileges. It is thus necessary to audit its activities and usage.'
 
-  tag 'host'
-  tag 'cis-docker-1.12.0': '1.7'
-  tag 'cis-docker-1.13.0': '1.5'
-  tag 'level:1'
-  ref 'System auditing', url: 'https://access.redhat.com/documentation/en-US/Red_Hat_Enterprise_Linux/6/html/Security_Guide/chap-system_auditing.html'
-
   only_if { os.linux? }
   describe service('auditbeat') do
     it { should be_installed }
@@ -102,16 +87,6 @@ control 'host-1.4' do
 
   Rationale: Tagged images are useful to fall back from "latest" to a specific version of an image in production. Images with unused or old tags may contain vulnerabilities that might be exploited, if instantiated. Additionally, if you fail to remove unused images from the system and there are various such redundant and unused images, the host filesystem may become full and could lead to denial of service.'
 
-  tag 'host'
-  tag 'cis-docker-1.12.0': '1.4'
-  tag 'cis-docker-1.13.0': '1.4'
-  tag 'level:1'
-  ref 'Clean up unused Docker Containers and Images', url: 'http://craiccomputing.blogspot.de/2014/09/clean-up-unused-docker-containers-and.html'
-  ref 'Command to remove all unused images', url: 'https://forums.docker.com/t/command-to-remove-all-unused-images/20/8'
-  ref 'docker rmi --unused', url: 'https://github.com/moby/moby/issues/9054'
-  ref 'Use the Docker command line', url: 'https://docs.docker.com/engine/reference/commandline/cli/'
-  ref 'Add support for referring to images by digest', url: 'https://github.com/moby/moby/pull/11109'
-
   instantiated_images = command('docker ps -qa | xargs docker inspect -f \'{{.Image}}\'').stdout.split
   all_images = command('docker images -q --no-trunc').stdout.split
   diff = all_images - instantiated_images
@@ -127,13 +102,6 @@ control 'host-1.5' do
   desc 'Do not keep a large number of containers on the same host.
 
   Rationale: The flexibility of containers makes it easy to run multiple instances of applications and indirectly leads to Docker images that exist at varying security patch levels. It also means that you are consuming host resources that otherwise could have been used for running \'useful\' containers. Having more than just the manageable number of containers on a particular host makes the situation vulnerable to mishandling, misconfiguration and fragmentation. Thus, avoid container sprawl and keep the number of containers on a host to a manageable total.'
-
-  tag 'host'
-  tag 'cis-docker-1.12.0': '1.5'
-  tag 'cis-docker-1.13.0': '1.5'
-  tag 'level:1'
-  ref 'Security Risks and Benefits of Docker Application Containers', url: 'https://zeltser.com/security-risks-and-benefits-of-docker-application/'
-  ref 'Docker networking: How Linux containers will change your network', url: 'http://searchsdn.techtarget.com/feature/Docker-networking-How-Linux-containers-will-change-your-network'
 
   total_on_host = command('docker info').stdout.split[1].to_i
   total_running = command('docker ps -q').stdout.split.length
